d441087a25ad4d3d402d59470f7ee0da9576ce15
enidesserts <eni.desserts@gmail.com>
2025-06-09 09:05:38 -0700

    remove unused js style

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import './App.css';
 
 function App() {
-   const style = {
-    backgroundImage: 'url(/Cream.jpg)', // Path to the image in the public folder
-    backgroundSize: 'cover', // Makes sure the image covers the entire container
-    backgroundPosition: 'center', // Centers the image
-    height: '100vh', // Takes up full viewport height
-    width: '100%', // Full width
-  };
   return (
       <div className="landing-page-container">
         <img
