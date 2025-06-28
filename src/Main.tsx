c3709a30ee3d2d1851da72544264ff476c5aeb20
enidesserts <eni.desserts@gmail.com>
2025-06-27 19:40:33 -0700

    Add cookie features

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,6 +4,7 @@ import { AboutUs } from "./aboutus/AboutUs";
 import { TopBar } from "./TopBar";
 import { CakeFeatures } from "./cakefeatures/CakeFeatures";
 import { Location } from "./location/Location";
+import { CookieFeatures } from "./cookiefeatures/CookieFeatures";
 
 export function Main(){
     return(
@@ -17,6 +18,8 @@ export function Main(){
             <CakeFeatures/>
             <Divider/>
             <Location/>
+            <Divider titleElement={<div style={{color: '#EA7E42'}}>JUNE CAKE FEATURES</div>}/>
+            <CookieFeatures/>
         </div>
     );
 }
