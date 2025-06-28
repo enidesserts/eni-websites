7cf704350e4bdd8ed9a8d653fdae43978906e24e
enidesserts <eni.desserts@gmail.com>
2025-06-27 19:57:37 -0700

    Move topbar into components

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,7 @@
 import { Overview } from "./overview/Overview";
 import { Divider } from "./components/Divider";
 import { AboutUs } from "./aboutus/AboutUs";
-import { TopBar } from "./TopBar";
+import { TopBar } from "./components/TopBar";
 import { CakeFeatures } from "./cakefeatures/CakeFeatures";
 import { Location } from "./location/Location";
 import { CookieFeatures } from "./cookiefeatures/CookieFeatures";
