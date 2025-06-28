2d96074fc9db5420c102616ff7b64a834aecdb28
enidesserts <eni.desserts@gmail.com>
2025-06-27 18:50:22 -0700

    Update overview card

--- src/overview/Overview.tsx
+++ src/overview/Overview.tsx
@@ -1,7 +1,11 @@
+import './Overview.scss';
+
 export function Overview(){
     return (
-      <div className="overview-body">
-          DESSERTS FOR ENI OCCASION
+      <div className="overview">
+          <div className="content">
+            Cakes and pastries <br/> for eni occasion
+          </div>
       </div>
   );
 }
