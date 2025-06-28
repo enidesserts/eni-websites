99d2ede8e82a85828e75f96f664e3560c5f37cd3
enidesserts <eni.desserts@gmail.com>
2025-06-27 19:27:13 -0700

    Add cake features placeholder page

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,6 +2,7 @@ import { Overview } from "./overview/Overview";
 import { Divider } from "./components/Divider";
 import { AboutUs } from "./aboutus/AboutUs";
 import { TopBar } from "./TopBar";
+import { CakeFeatures } from "./cakefeatures/CakeFeatures";
 
 export function Main(){
     return(
@@ -12,6 +13,7 @@ export function Main(){
             <Divider/>
             <AboutUs/>
             <Divider titleElement={<div style={{color: '#9DB652'}}>JUNE CAKE FEATURES</div>}/>
+            <CakeFeatures/>
         </div>
     );
 }
