def18bddc8dbfdb54413de333a530ff7c9797a6c
enidesserts <eni.desserts@gmail.com>
2025-07-16 13:28:05 -0700

    Update overview page to use Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,26 @@ import './Card.scss';
 /**
  * A reusable component for each section of the website. It keeps padding for the dividers
  */
-export function Card({children, className}: {children: React.ReactNode; className?: string}) {
+export function Card({
+  children,
+  className,
+  backgroundImagePath,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  backgroundImagePath?: string;
+}) {
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{
+        ...(backgroundImagePath && {
+          backgroundImage: `url(${backgroundImagePath})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }),
+      }}
+    >
       <div className={`card-content ${className}`}>{children}</div>
     </div>
   );
