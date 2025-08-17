333f9da77ea6219a48bfac2cc2c305de68749e44
Ian Jiang <2487958+jyyae86@users.noreply.github.com>
2025-08-16 22:18:22 -0400

    add url to order button

--- src/components/OrderButton.tsx
+++ src/components/OrderButton.tsx
@@ -2,8 +2,11 @@ import './OrderButton.scss';
 
 export function OrderButton() {
   return (
-    <div className="floating-order-button">
+    <a
+      className="floating-order-button"
+      href="https://www.cognitoforms.com/EniDesserts2/EniDessertsPreOrderForm"
+    >
       <img className="image" src="OrderNow.png" alt="Order Now" />
-    </div>
+    </a>
   );
 }
