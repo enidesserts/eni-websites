25c29795091b9de1db7c6d6cd0c6fce68771e8a6
enidesserts <eni.desserts@gmail.com>
2026-07-15 17:14:13 -0700

    Revert "Reorganized menuitems folder. Added in October menu and changed Menu.tsx"
    This reverts commit 728c5bd52d7710c10aebf0a2229867030ba45044.

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -11,34 +11,59 @@ type MenuItem = {
 export default function Menu() {
   const cookieMenuItems: MenuItem[] = [
     {
-      name: 'Ube Leche Flan',
+      name: 'Cheddar Cheese Ganache Cookie',
       price: '$4.50',
-      description: 'Ube cookie, ube halaya, leche flan',
+      description:
+        'Cheese cookie with a cheese white chocolate ganache, topped with a cheese cracker',
 
-      imagePath: './menuitems/Cookie/ube_cookie.png',
+      imagePath: './menuitems/CheddarCheeseGanacheCookie.png',
     },
     {
-      name: 'Thai Tea Ganache',
+      name: 'Matcha Chocolate Ganache Cookie',
       price: '$4.50',
-      description: 'Thai tea cookie, thai tea ganache',
-
-      imagePath: './menuitems/Cookie/thaitea_cookie.png',
+      description: 'Matcha and chocolate cookie with a matcha white chocolate ganache',
+      imagePath: './menuitems/MatchaChocolateGanacheCookie.png',
+    },
+    {
+      name: 'Chocolate Chip Cookie',
+      price: '$3.50',
+      description: 'Your classic cookie with milk and dark chocolate chunks',
+      imagePath: './menuitems/ChocolateChipCookie.png',
+    },
+    {
+      name: 'Eni Cookie Trio',
+      price: '$12',
+      description: 'Bundle the three cookies, perfect for sharing ',
+      imagePath: './menuitems/CookieTrio.png',
     },
   ];
 
+  // cake menu
   const cakeMenuItems: MenuItem[] = [
     {
-      name: 'Ube Cream Cheese',
-      price: '$10',
-      description: 'Ube sponge cake, cream cheese mousse, ube whipped ganache, leche flan',
-      imagePath: './menuitems/CakeBox/ube_cakebox.png',
-    },
-    {
-      name: 'Coconut Pandan',
+      name: 'Strawberry Earl Grey Cake Box',
       price: '$10',
       description:
-        'Pandan sponge cake, coconut whipped ganache, macapuno strings, pandan jellies, toasted coconut',
-      imagePath: './menuitems/CakeBox/pandan_cakebox.png',
+        'Earl grey whipped ganache, strawberry cremeux, fresh strawberries, chiffon cake',
+      imagePath: './menuitems/StrawberryEarlGreyCakeBox.png',
+    },
+    {
+      name: 'Green Grape Jasmine Cake Box',
+      price: '$10',
+      description: 'Jasmine whipped ganache, jasmine jelly, fresh grapes, chiffon cake',
+      imagePath: './menuitems/GreenGrapeJasmineCakeBox.png',
+    },
+    {
+      name: 'Mango Matcha Cake Box',
+      price: '$10',
+      description: 'Matcha whipped ganache, fresh mangoes, chiffon cake',
+      imagePath: './menuitems/MangoMatchaCakeBox.png',
+    },
+    {
+      name: 'Lychee Genmaicha Cake Box',
+      price: '$10',
+      description: 'Genmaicha whipped ganache, lychees, coconut jelly, chiffon cake',
+      imagePath: './menuitems/LycheeGenmaichaCakeBox.png',
     },
   ];
 
